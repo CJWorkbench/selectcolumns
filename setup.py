531598deea72a55f298100a6e090ab562b67bc3e
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2020-02-18 13:08:14 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="selectcolumns",
+    version="0.0.1",
+    description="Retain or delete columns from the table",
+    author="Adam Hooper",
+    author_email="adam@adamhooper.com",
+    url="https://github.com/CJWorkbench/selectcolumns",
+    packages=[""],
+    py_modules=["selectcolumns"],
+    install_requires=["pandas==0.25.0"],
+)
